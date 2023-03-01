abfc01093cb3d81009290331f91b7498defa2b22
Christina C <83510714+christina-clifton@users.noreply.github.com>
2023-03-01 13:57:41 -0800

    attempt to make React Router compatible with gh-pages

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider basename="/peas-carrots" router={router} />
   </React.StrictMode>
 );
 
